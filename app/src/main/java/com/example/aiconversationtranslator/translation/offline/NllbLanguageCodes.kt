cc1c795a69ac610bf4c51286ba6cae7c2d50d080
iftikharjutt <ilink4ai@gmail.com>
2026-08-31 16:23:01 +0800

    feat: add NLLB Malay and Urdu language mapping

---
+++ app/src/main/java/com/example/aiconversationtranslator/translation/offline/NllbLanguageCodes.kt
@@ -0,0 +1,7 @@
+package com.example.aiconversationtranslator.translation.offline
+
+/** NLLB FLORES-200 language codes used by the offline translator. */
+object NllbLanguageCodes {
+    const val MALAY = "msa_Latn"
+    const val URDU = "urd_Arab"
+}
